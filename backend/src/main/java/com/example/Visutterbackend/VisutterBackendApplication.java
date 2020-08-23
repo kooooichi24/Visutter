04412cd77a492da1e787458e8ed83c51e8e38a82
kooooichi24 <ex.kouichi1224@gmail.com>
2020-08-23 20:30:59 +0900

    backend first comimt

---
+++ backend/src/main/java/com/example/Visutterbackend/VisutterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Visutterbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisutterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisutterBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/Visutterbackend/VisutterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Visutterbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisutterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
