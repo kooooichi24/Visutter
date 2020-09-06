d20651df63a674d891e65a02d7c23f60f8a83bf7
kooooichi24 <ex.kouichi1224@gmail.com>
2020-09-06 21:21:15 +0900

    Hello worldファイルを型対応しました。

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -94,60 +94,73 @@
 <script lang="ts">
   import Vue from 'vue'
 
+  type Data = {
+    text: string;
+    href: string;
+  }
+  
+  type DataType = {
+    ecosystem: Data[];
+    importantLinks: Data[];
+    whatsNext: Data[];
+  }
+
   export default Vue.extend({
     name: 'HelloWorld',
 
-    data: () => ({
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader',
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify',
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify',
-        },
-      ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com',
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com',
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuejs.com/vuetify',
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs',
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify',
-        },
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer',
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-        },
-      ],
-    }),
+    data(): DataType {
+      return {
+        ecosystem: [
+          {
+            text: 'vuetify-loader',
+            href: 'https://github.com/vuetifyjs/vuetify-loader',
+          },
+          {
+            text: 'github',
+            href: 'https://github.com/vuetifyjs/vuetify',
+          },
+          {
+            text: 'awesome-vuetify',
+            href: 'https://github.com/vuetifyjs/awesome-vuetify',
+          },
+        ],
+        importantLinks: [
+          {
+            text: 'Documentation',
+            href: 'https://vuetifyjs.com',
+          },
+          {
+            text: 'Chat',
+            href: 'https://community.vuetifyjs.com',
+          },
+          {
+            text: 'Made with Vuetify',
+            href: 'https://madewithvuejs.com/vuetify',
+          },
+          {
+            text: 'Twitter',
+            href: 'https://twitter.com/vuetifyjs',
+          },
+          {
+            text: 'Articles',
+            href: 'https://medium.com/vuetify',
+          },
+        ],
+        whatsNext: [
+          {
+            text: 'Explore components',
+            href: 'https://vuetifyjs.com/components/api-explorer',
+          },
+          {
+            text: 'Select a layout',
+            href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
+          },
+          {
+            text: 'Frequently Asked Questions',
+            href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
+          },
+        ], 
+      }
+    },
   })
 </script>
