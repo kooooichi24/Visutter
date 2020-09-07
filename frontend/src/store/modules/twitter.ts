bc78c97607665addfbc8553660b72e6c3daba537
kooooichi24 <ex.kouichi1224@gmail.com>
2020-09-08 00:58:49 +0900

    ボタンクリックでtimeline取得できるようになった

--- frontend/src/store/modules/twitter.ts
+++ frontend/src/store/modules/twitter.ts
@@ -1,24 +1,53 @@
-import { Module, GetterTree } from 'vuex';
-import { TwitterState, RootState } from '@/store/types';
+import { Module, GetterTree, ActionTree, MutationTree } from 'vuex';
+import axios from 'axios';
+import { TwitterState, RootState, Tweet } from '@/store/types';
 
 const state: TwitterState = {
-  tweet: {
-    id: 1302758543519559700,
-    text: "2020-09-06: 1 tweet",
-    createdAt: "2020-09-07T09:00:15.000+09:00",
-    favoriteCount: 0,
-    retweetCount: 0
-  }
-}
+  tweets: [
+    {
+      id: 1302758543519559700,
+      text: "2020-09-06: 1 tweet",
+      createdAt: "2020-09-07T09:00:15.000+09:00",
+      favoriteCount: 0,
+      retweetCount: 0
+    },
+  ],
+  timeline: [],
+};
 
 const getters: GetterTree<TwitterState, RootState> = {
   tweets: (state: TwitterState) => {
-    return state.tweet;
+    return state.tweets;
+  },
+  timeline: (state: TwitterState) => {
+    return state.timeline;
   },
 };
+
+const mutations: MutationTree<TwitterState> = {
+  addTweet: (state, tweet: Tweet) => {
+    state.tweets.push(tweet);
+  },
+  setTimeline: (state, timeline: Tweet[]) => {
+    state.timeline = timeline;
+  }
+}
+
+const actions: ActionTree<TwitterState, RootState> = {
+  addTweet: ({ commit }, tweet: Tweet) => {
+    commit('addTweet', tweet);
+  },
+  setTimeline: async ({ commit }) => {
+    const timeline = await axios({ method: 'GET', url: 'http://localhost:8080/api/twt/timeline' }).catch((error) => error)
+    commit('setTimeline', timeline.data);
+  }
+};
+
 
 export const twitter: Module<TwitterState, RootState> = {
   namespaced: true,
   state,
-  getters
+  getters,
+  mutations,
+  actions
 };
--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <v-btn rounded color="primary" dark @click="getTimeline" >Home</v-btn>
     <CalendarHeatmap />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import axios from 'axios';
 import CalendarHeatmap from '@/components/CalendarHeatmap.vue';
 
 export default Vue.extend({
@@ -16,13 +16,12 @@ export default Vue.extend({
     CalendarHeatmap,
   },
   created(): void {
-    axios
-      .get("http://localhost:8080/api/twt/timeline")
-      .then(res => {
-        console.log(res.data);
-      });
-
-    console.log(this.$store.getters["twitter/tweets"]);
+    this.$store.dispatch('twitter/setTimeline')
+  },
+  methods: {
+    getTimeline(): void {
+      console.log(this.$store.getters["twitter/timeline"]);
+    }
   }
 });
 </script>
