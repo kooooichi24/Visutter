595bdf8fc7128a9f9492c329935576758f7f1d56
kooooichi24 <ex.kouichi1224@gmail.com>
2020-09-22 23:52:38 +0900

    vue-charts と typescript のエラーでこけてる

---
+++ frontend/src/components/Chart.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    This is chart components.
+    <LineChart />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import LineChart from '@/components/LineChart.vue'
+
+export default Vue.extend({
+  name: 'Chart',
+  components: {
+    LineChart
+  },
+});
+</script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <Achievement />
+    <Chart />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import Achievement from '@/components/Achievement.vue';
+import Chart from '@/components/Chart.vue';
 
 export default Vue.extend({
   name: 'Home',
   components: {
     Achievement,
+    Chart
   },
 });
 </script>
