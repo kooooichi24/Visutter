d38f3cab173a34e541983f5798e473f9ed14e59d
kooooichi24 <ex.kouichi1224@gmail.com>
2020-09-27 23:34:56 +0900

    RandomChartをts化

--- frontend/src/components/RandomChart.vue
+++ frontend/src/components/RandomChart.vue
@@ -5,47 +5,54 @@
   </div>
 </template>
 
-<script>
-  import LineChart from './LineChart.vue'
+<script lang="ts">
+import Vue from 'vue';
+import LineChart from './LineChart.vue'
+import { ChartData } from "chart.js";
 
-  export default {
-    components: {
-      LineChart
-    },
-    data () {
-      return {
-        datacollection: null
+type DataType = {
+  datacollection: ChartData | null;
+}
+
+export default Vue.extend({
+  name: 'RandomChart',
+  components: {
+    LineChart
+  },
+  data(): DataType {
+    return {
+      datacollection: null
+    }
+  },
+  mounted(): void {
+    this.fillData()
+  },
+  methods: {
+    fillData(): void {
+      this.datacollection = {
+        labels: [this.getRandomInt(), this.getRandomInt()],
+        datasets: [
+          {
+            label: 'Data One',
+            backgroundColor: '#007900',
+            data: [this.getRandomInt(), this.getRandomInt()]
+          },
+          {
+            label: 'Data One',
+            backgroundColor: '#f87979',
+            data: [this.getRandomInt(), this.getRandomInt()]
+          }
+        ]
       }
     },
-    mounted () {
-      this.fillData()
-    },
-    methods: {
-      fillData () {
-        this.datacollection = {
-          labels: [this.getRandomInt(), this.getRandomInt()],
-          datasets: [
-            {
-              label: 'Data One',
-              backgroundColor: '#007900',
-              data: [this.getRandomInt(), this.getRandomInt()]
-            },
-            {
-              label: 'Data One',
-              backgroundColor: '#f87979',
-              data: [this.getRandomInt(), this.getRandomInt()]
-            }
-          ]
-        }
-      },
-      getRandomInt () {
-        return Math.floor(Math.random() * (50 - 5 + 1)) + 5
-      }
+    getRandomInt(): number {
+      return Math.floor(Math.random() * (50 - 5 + 1)) + 5
     }
   }
+});
 </script>
 
-<style>
+<style lang="scss" scoped>
   .small {
     max-width: 600px;
     margin:  150px auto;
