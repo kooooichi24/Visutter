06248f8242693d890ba074017a7d50dd3664b424
古川広一 <ex.kouichi1224@gmail.com>
2020-10-11 23:57:23 +0900

    vuexで不要なコードを削除

--- frontend/src/store/modules/twitter.ts
+++ frontend/src/store/modules/twitter.ts
@@ -3,24 +3,11 @@ import axios from 'axios';
 import { TwitterState, RootState, Tweet, User } from '@/store/types';
 
 const state: TwitterState = {
-  tweets: [
-    {
-      id: 1302758543519559700,
-      text: "2020-09-06: 1 tweet",
-      createdAt: "2020-09-07T09:00:15.000+09:00",
-      favoriteCount: 0,
-      retweetCount: 0,
-      isRetweeted: false,
-    },
-  ],
   timeline: [],
   user: [],
 };
 
 const getters: GetterTree<TwitterState, RootState> = {
-  tweets: (state: TwitterState) => {
-    return state.tweets;
-  },
   timeline: (state: TwitterState) => {
     return state.timeline;
   },
@@ -30,9 +17,6 @@ const getters: GetterTree<TwitterState, RootState> = {
 };
 
 const mutations: MutationTree<TwitterState> = {
-  addTweet: (state, tweet: Tweet) => {
-    state.tweets.push(tweet);
-  },
   setTimeline: (state, timeline: Tweet[]) => {
     state.timeline = timeline;
   },
@@ -42,9 +26,6 @@ const mutations: MutationTree<TwitterState> = {
 }
 
 const actions: ActionTree<TwitterState, RootState> = {
-  addTweet: ({ commit }, tweet: Tweet) => {
-    commit('addTweet', tweet);
-  },
   setTimeline: async ({ commit }, screenName: string) => {
     const timeline = await axios({ method: 'GET', url: `http://localhost:8080/api/twt/timeline?screenName=${screenName}` }).catch((error) => error)
     commit('setTimeline', timeline.data);
--- frontend/src/store/types.ts
+++ frontend/src/store/types.ts
@@ -3,7 +3,6 @@ export interface RootState {
 }
 
 export interface TwitterState {
-  tweets: Tweet[];
   timeline: Tweet[];
   user: User[];
 }
