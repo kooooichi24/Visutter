912bb39f35dba3752259c0133cc6f4af01e37cbb
古川広一 <ex.kouichi1224@gmail.com>
2020-10-20 23:15:38 +0900

    v-modelが連動していない問題を修正しました

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -145,8 +145,8 @@ export default Vue.extend({
     }
   },
   watch: {
-    $route (to, from) {
-      this.model = this.items.findIndex(item => item.path === from.path);
+    $route (to) {
+      this.model = this.items.findIndex(item => item.path === to.path);
     }
   },
   created(): void {
